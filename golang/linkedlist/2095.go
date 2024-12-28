2c5f24a8b9576197e26205175bb263568982c406
Mahendra <mahendrachaudharycr7@gmail.com>
2024-12-28 22:16:45 +0530

    delete ll middle element

---
+++ golang/linkedlist/2095.go
@@ -0,0 +1,26 @@
+package main
+
+// ListNode Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func deleteMiddle(head *ListNode) *ListNode {
+
+	if head == nil || head.Next == nil {
+		return nil
+	}
+
+	slow := head
+	fast := head
+
+	for fast.Next != nil && fast.Next.Next != nil && fast.Next.Next.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+
+	slow.Next = slow.Next.Next
+
+	return head
+}
