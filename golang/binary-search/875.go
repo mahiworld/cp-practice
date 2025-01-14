40cdce5badd00e299eba2292389495ddbc26e94d
Mahendra <mahendrachaudharycr7@gmail.com>
2025-01-14 21:24:28 +0530

    koko eating bananas

---
+++ golang/binary-search/875.go
@@ -0,0 +1,31 @@
+package main
+
+// Koko Eating Bananas
+func minEatingSpeed(piles []int, h int) int {
+	kmin, kmax := 1, 1
+	for _, ele := range piles {
+		if ele > kmax {
+			kmax = ele
+		}
+	}
+
+	if len(piles) == h {
+		return kmax
+	}
+
+	for kmin < kmax {
+		mid := (kmin + kmax) / 2
+
+		totalHours := 0
+		for _, ele := range piles {
+			totalHours += (ele + mid - 1) / mid
+		}
+		if totalHours <= h {
+			kmax = mid
+		} else {
+			kmin = mid + 1
+		}
+	}
+
+	return kmin
+}
