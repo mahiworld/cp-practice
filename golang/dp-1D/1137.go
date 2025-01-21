2260eb1e9af6a78353e5b6e9e8e13fc8b16388e5
Mahendra <mahendrachaudharycr7@gmail.com>
2025-01-21 21:30:13 +0530

    iterative approach

--- golang/dp-1D/1137.go
+++ golang/dp-1D/1137.go
@@ -3,12 +3,30 @@ package main
 //N-th Tribonacci Number
 
 // Approach:1 Recursion ------------------------
+// func tribonacci(n int) int {
+// 	if n == 0 {
+// 		return 0
+// 	} else if n == 1 || n == 2 {
+// 		return 1
+// 	}
+
+// 	return tribonacci(n-3) + tribonacci(n-2) + tribonacci(n-1)
+// }
+
+// Apprach:2 Iterative ---------------------------
 func tribonacci(n int) int {
 	if n == 0 {
 		return 0
-	} else if n == 1 || n == 2 {
+	}
+	if n == 1 || n == 2 {
 		return 1
 	}
 
-	return tribonacci(n-3) + tribonacci(n-2) + tribonacci(n-1)
+	t0, t1, t2 := 0, 1, 1
+	for i := 3; i <= n; i++ {
+		t3 := t0 + t1 + t2
+		t0, t1, t2 = t1, t2, t3
+	}
+
+	return t2
 }
