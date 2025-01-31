35fabea8fc9aca7356cd0e3695ebbae3272ac5fc
Mahendra <mahendrachaudharycr7@gmail.com>
2025-01-31 21:56:01 +0530

    dp approach

--- golang/bit-manipulation/338.go
+++ golang/bit-manipulation/338.go
@@ -19,11 +19,13 @@ package main
 // 	return strings.Count(binaryStr, "1")
 // }
 
-// Approach : 2 ----------------------------------
+// Approach : 2 -> Dynamic programming approach
 func countBits(n int) []int {
 	resp := make([]int, n+1)
 
 	for i := 1; i <= n; i++ {
+		// i >> 1 shifts the bits right, effectively removing the least significant bit
+		// i & 1 checks whether the least significant bit is 1
 		resp[i] = resp[i>>1] + (i & 1)
 	}
 
