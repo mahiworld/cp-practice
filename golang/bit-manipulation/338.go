b07846a6019689d721ebd6bcb6144d4b7528325d
Mahendra <mahendrachaudharycr7@gmail.com>
2025-01-31 21:50:55 +0530

    right shift

--- golang/bit-manipulation/338.go
+++ golang/bit-manipulation/338.go
@@ -1,25 +1,31 @@
 package main
 
-import (
-	"strconv"
-	"strings"
-)
-
 // Counting Bits
 
 // Approach : 1 -------------------------------
-func countBits(n int) []int {
-	resp := make([]int, 0)
+// func countBits(n int) []int {
+// 	resp := make([]int, 0)
 
-	for i := 0; i <= n; i++ {
-		resp = append(resp, countBinaryOnes(i))
+// 	for i := 0; i <= n; i++ {
+// 		resp = append(resp, countBinaryOnes(i))
+// 	}
+
+// 	return resp
+// }
+
+// func countBinaryOnes(num int) int {
+// 	binaryStr := strconv.FormatInt(int64(num), 2)
+
+// 	return strings.Count(binaryStr, "1")
+// }
+
+// Approach : 2 ----------------------------------
+func countBits(n int) []int {
+	resp := make([]int, n+1)
+
+	for i := 1; i <= n; i++ {
+		resp[i] = resp[i>>1] + (i & 1)
 	}
 
 	return resp
-}
-
-func countBinaryOnes(num int) int {
-	binaryStr := strconv.FormatInt(int64(num), 2)
-
-	return strings.Count(binaryStr, "1")
 }
