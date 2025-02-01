ffefd428f271f146f736bca4f72e3214f650d4f3
Mahendra <mahendrachaudharycr7@gmail.com>
2025-02-01 22:09:07 +0530

    xor approach

--- golang/bit-manipulation/136.go
+++ golang/bit-manipulation/136.go
@@ -1,21 +1,31 @@
 package main
 
 // Single Number
+// func singleNumber(nums []int) int {
+// 	visited := make(map[int]bool)
+// 	for _, num := range nums {
+// 		if visited[num] {
+// 			visited[num] = false
+// 		} else {
+// 			visited[num] = true
+// 		}
+// 	}
+
+// 	for key, value := range visited {
+// 		if value {
+// 			return key
+// 		}
+// 	}
+
+//		return -1
+//	}
+
 func singleNumber(nums []int) int {
-	visited := make(map[int]bool)
+	var resp int
+
 	for _, num := range nums {
-		if visited[num] {
-			visited[num] = false
-		} else {
-			visited[num] = true
-		}
+		resp ^= num
 	}
 
-	for key, value := range visited {
-		if value {
-			return key
-		}
-	}
-
-	return -1
+	return resp
 }
